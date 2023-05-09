b85b9edf02a7e692a2cd0d05bbccf632668499a0
progali007 <code.progali@gmail.com>
2023-05-09 16:13:23 +0430

    :wrench: (form-generator): Update types for FormGenerator component

--- src/models/form-generator.ts
+++ src/models/form-generator.ts
@@ -2,15 +2,56 @@ export type FormGeneratorInput = {
   width: "full" | "normal";
   id: string;
   name: string;
-  type: "button" | "checkbox" | "color" | "date" | "datetime-local" | "email" | "file" | "hidden" | "image" | "month" | "number" | "password" | "radio" | "range" | "reset" | "search" | "submit" | "tel" | "text" | "time" | "url" | "week";
   placeholder: string;
+  title: string;
+  type:
+    | "button"
+    | "checkbox"
+    | "color"
+    | "date"
+    | "datetime-local"
+    | "email"
+    | "file"
+    | "hidden"
+    | "image"
+    | "month"
+    | "number"
+    | "password"
+    | "radio"
+    | "range"
+    | "reset"
+    | "search"
+    | "submit"
+    | "tel"
+    | "text"
+    | "time"
+    | "url"
+    | "week";
+};
+
+export type FormGeneratorRadio = {
+  id: string;
+  name: string;
+  value: string;
+  title: string;
 };
 
 export type InputWidth = {
   full: number;
   normal: number;
-}
+};
 
 export type FormGeneratorProps = {
-  jsonFile: FormGeneratorInput[];
+  jsonFile: {
+    inputs: FormGeneratorInput[];
+    radios: {
+      groups: {
+        [key: string]: {
+          id: string;
+          title: string;
+          data: FormGeneratorRadio[];
+        };
+      };
+    };
+  };
 };
