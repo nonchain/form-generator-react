5052f22bcbe35c2a4566b7f563450f140ba4c95f
progali007 <code.progali@gmail.com>
2023-05-10 18:55:30 +0430

    :sparkles: (App): Implement the FormGenerator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // Components
-import { Stack } from "@chakra-ui/react";
-import FormGenerator from "./components/form-generator/form-generator"
+import { Box, Stack } from "@chakra-ui/react";
+import FormGenerator from "./components/form-generator/form-generator";
 // Files
 import formJson from "./dummy-form-json.json";
 
 function App() {
   return (
     <Stack minHeight="100vh" alignItems="center" justifyContent="center">
-      <FormGenerator jsonFile={formJson}/>
+      <Box width="40%">
+        <FormGenerator jsonFile={formJson} />
+      </Box>
     </Stack>
-  )
+  );
 }
 
-export default App
+export default App;
